67c07961c8d2a21677572fd042d941fb61f0acce
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2023-04-04 09:35:08 -0500

    Add navigation components
    - Add the navigation components dependencies
    - Create the recipes nav graph to handle the fragments
    - Change to databinding feature to handle the view references
    - Add to project the fragments that will be used across the navigations. Recipes, Favorites, Food Joke and Maps
    - Add the drawables to the bottom navigation view
    - Add some new strings to the project

---
+++ app/src/main/java/com/sandoval/recipesapp/fragments/RecipesFragment.kt
@@ -0,0 +1,21 @@
+package com.sandoval.recipesapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sandoval.recipesapp.R
+
+class RecipesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    }
+
+}
