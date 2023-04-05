4d89818d33e447e30d797be14515079060e7b425
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2023-04-05 14:00:08 -0500

    Apply the filter to the recipes list and save the state
    - Add the safeArgs dependency to handle the arguments to the bottomsheet and to the recipes list
    - Implement in the bottomsheet fragment the filter implementation and save the state
    - Add to the readDatabas the state saved from the bottomsheet filters to keep it
    - Add the arguments to the navgraph
    -  Refactor the gradle to handle the datastore dependency in the versions toml catalog

--- app/src/main/java/com/sandoval/recipesapp/ui/list_recipes/fragments/RecipesFragment.kt
+++ app/src/main/java/com/sandoval/recipesapp/ui/list_recipes/fragments/RecipesFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sandoval.recipesapp.R
 import com.sandoval.recipesapp.databinding.FragmentRecipesBinding
@@ -23,6 +24,8 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private val args: RecipesFragmentArgs by navArgs()
 
     private lateinit var mainViewModel: MainViewModel
     private lateinit var recipesViewModel: RecipesViewModel
@@ -66,7 +69,7 @@ class RecipesFragment : Fragment() {
     private fun readDatabase() {
         lifecycleScope.launch {
             mainViewModel.readRecipes.observeOnce(viewLifecycleOwner) { database ->
-                if (database.isNotEmpty()) {
+                if (database.isNotEmpty() && !args.backFromBottomSheet) {
                     Log.d("RecipesFragment", "readDatabase called!")
                     mAdapter.setData(database[0].foodRecipe)
                     hideShimmerEffect()
